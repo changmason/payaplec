78e9ced68afe57bc63049ac417e55bc4a6a9f62e
mason <mason@dreamerslab.com>
2012-06-13 17:57:09 +0800

    add controller Cart for checkout

---
+++ app/controllers/carts.js
@@ -0,0 +1,32 @@
+var Application = require( CONTROLLER_DIR + 'application' );
+var qs          = require( 'querystring' );
+var request     = require( 'request' );
+
+var api_url = 'https://api-3t.sandbox.paypal.com/nvp';
+var params  = {
+  USER      : 'seller_1339472528_biz_api1.gmail.com',
+  PWD       : '1339472553',
+  SIGNATURE : 'AFcWxV21C7fd0v3bYYYRCpSSRl31Af-aECo8vsiP1HospgIyBCFncbx3',
+  METHOD    : 'SetExpressCheckout',
+  VERSION   : '78',
+  returnUrl : 'http://localhost:4000/return',
+  cancelUrl : 'http://localhost:4000/cancel',
+  PAYMENTREQUEST_0_AMT          : '11.99',
+  PAYMENTREQUEST_0_CURRENCYCODE : 'USD',
+  PAYMENTREQUEST_0_DESC         : 'Sashimi'
+};
+
+module.exports = Application.extend({
+
+  checkout : function ( req, res, next ){
+    var url = api_url + '?' + qs.stringify( params );
+
+    request.get( url, function ( err, response, body ){
+      if( err ) next( err );
+
+      var token = qs.parse( body )[ 'TOKEN' ];
+
+      res.render( 'carts/checkout', { token : token });
+    });
+  }
+});
--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,5 @@
 module.exports = function ( map ){
   map.get( '/','welcome#index' );
+
+  map.get( '/carts/checkout', 'carts#checkout' );
 };
